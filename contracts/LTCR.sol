6942517c66ab0ab218b60a5246f6c75e2ee8609f
Dominik Harz <dominik.harz@gmail.com>
2019-03-27 17:41:24 +0900

    Refactor the udpate and curate functions

--- contracts/LTCR.sol
+++ contracts/LTCR.sol
@@ -15,7 +15,9 @@ contract LTCR is Ownable {
     mapping (address => uint256) _scores;
     mapping (address => uint256) _deposits;
     mapping (uint256 => uint256) _rewards;
-    address[] _agents; // track which agents need updating
+    
+    address[] _agents; // track all agents
+    mapping (address => bool) _remain; // default to false
 
     uint256 _blockperiod;
     uint256 _start;
@@ -25,6 +27,8 @@ contract LTCR is Ownable {
         _decimals = 3; // e.g. a factor of 1500 is equal to 1.5 times the collateral
         // wait for 10 blocks to reorg
         _blockperiod = 1;
+        _start = block.number;
+        _end = block.number + _blockperiod;
     }
     
     // ##############
@@ -112,7 +116,7 @@ contract LTCR is Ownable {
         return _assignments[agent];
     }
 
-    function getAgentColalteral(address agent) public view returns (uint256) {
+    function getAgentCollateral(address agent) public view returns (uint256) {
         require(_assignments[agent] > 0, "agent not assigned to layer");
         return _deposits[agent];
     }
@@ -124,9 +128,15 @@ contract LTCR is Ownable {
 
     function registerAgent(address agent, uint256 collateral) public returns (bool) {
         require(collateral >= _minCollateral * (_factors[_layers[0]] / (10 ** _decimals)), "too little collateral");
+        
+        // asign agent to lowest layer
         _assignments[agent] = _layers[0];
+        // track the deposit
         _deposits[agent] = collateral;
-        recordAction(agent, 0);
+        // add agent to list to change in next round
+        _agents.push(agent);
+        // update the score of the agent
+        _scores[agent] += _rewards[0];
         
         emit RegisterAgent(agent, collateral);
         
@@ -135,34 +145,27 @@ contract LTCR is Ownable {
 
     event RegisterAgent(address agent, uint256 collateral);
 
-    function recordAction(address agent, uint256 action) public returns (bool) {
-        _scores[agent] += _rewards[action];
-
-        if (_scores[agent] > _upper[_assignments[agent]]) {
-            _agents.push(agent);
-        }
-        
-        return true;
-    }
-
-
     // ####################
     // ### TCR CONTROLS ###
     // ####################
 
-    function startPeriod() public onlyOwner returns (bool) {
-        require(_start == 0, "only for initial setup");
-        _start = block.number;
-        _end = block.number + _blockperiod;
+    function update(address agent, uint256 action) public returns (bool) {
+        _scores[agent] += _rewards[action];
 
-        emit StartedPeriod(_start, _end);
+        if (_scores[agent] >= _lower[_assignments[agent]] && _scores[agent] <= _upper[_assignments[agent]]) {
+            _remain[agent] = true;
+        } else {
+            _remain[agent] = false;
+        }
+        
+        emit Update(agent, _rewards[action], _scores[agent]);
 
         return true;
     }
 
-    event StartedPeriod(uint256 start, uint end);
+    event Update(address agent, uint256 reward, uint256 score);
 
-    function updateRanking() public onlyOwner returns (bool) {
+    function curate() public onlyOwner returns (bool) {
         require(_start != 0, "period not started");
         require(block.number >= _end, "period not ended");
 
@@ -170,29 +173,28 @@ contract LTCR is Ownable {
         _start = block.number;
         _end = block.number + _blockperiod;
 
-        emit UpdatedRanking(_agents, _start, _end);
+        emit Curate(_agents, _start, _end);
 
         // promote or demote _agents
-        while (_agents.length > 0) {
-            uint i = _agents.length - 1;
-            uint256 score = _scores[_agents[i]];
-            uint8 layer = _assignments[_agents[i]];
+        for (uint i = 0; i < _agents.length; i++) {
+            if (_remain[_agents[i]] == false) {
+                uint256 score = _scores[_agents[i]];
+                uint8 layer = _assignments[_agents[i]];
 
-            if (layer > 0 && score < _lower[layer]) { // lower bound check
-                _assignments[_agents[i]] -= 1;
-            } else if (layer < (_layers.length - 1) && score > _upper[layer]) { // upper bound check
-                _assignments[_agents[i]] += 1;
+                // lower bound check
+                if (layer > 0 && score < _lower[layer]) { 
+                    _assignments[_agents[i]] -= 1;
+                // upper bound check
+                } else if (layer < (_layers.length - 1) && score > _upper[layer]) {
+                    _assignments[_agents[i]] += 1;
+                }
             }
-
             // reset score
             _scores[_agents[i]] = 0;
-
-            // pop the last element
-            _agents.pop();
         }
 
         return true;
     }
 
-    event UpdatedRanking(address[] _agents, uint256 start, uint256 end);
+    event Curate(address[] _agents, uint256 start, uint256 end);
 }
